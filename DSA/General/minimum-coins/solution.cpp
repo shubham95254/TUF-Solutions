cd62586e3f75c3c3bac84c43cf9ae5d42b3891fe
Shubham <125039017+shubham95254@users.noreply.github.com>
2026-08-04 13:31:17 +0530

    Add solution for Minimum coins - TUFHub

--- DSA/General/minimum-coins/solution.cpp
+++ DSA/General/minimum-coins/solution.cpp
@@ -1,35 +1,17 @@
 class Solution{
 	public:
-    int f(int ind, int totalrem, vector<int>&coins, vector<vector<int>>&dp){
-        // if(ind<0) return 100000;
-        // if(totalrem==0) return 0;
-
-        dp[0][0] = 0;
-        for(int j=1; j<=totalrem; j++){
-            dp[0][j] = (j%coins[0]==0)? j/coins[0] : 1e9;
-        }
-        
-        // if(dp[ind][totalrem]!=-1) return dp[ind][totalrem];
-        for(int i=1; i<=ind; i++) {
-            dp[i][0] = 0;
-            for(int j=1; j<=totalrem; j++) {
-                int take = 100000;
-                if(coins[i]<=j) take = 1+dp[i][j-coins[i]];
-                int nottake = dp[i-1][j];
-
-                // cout << "f(" <<ind<<","<<totalrem<< ") " << take << " " << nottake<<endl;
-                dp[i][j]= min(take, nottake);
-            }
-        }
-        return dp[ind][totalrem];
-
-
-    }
 	int MinimumCoins(vector<int>& coins, int amount) {
-        vector<vector<int>> dp(coins.size(),vector<int>(amount+1, -1));
-        int ans = f(coins.size()-1, amount, coins, dp);
-        
-        if(ans>1000) return -1;
-        else return ans;
+    const int INF = 1e9;
+            vector<int> dp(amount + 1, INF);
+
+            dp[0] = 0;
+
+            for (int coin : coins) {
+                for (int j = coin; j <= amount; j++) {
+                    dp[j] = min(dp[j], 1 + dp[j - coin]);
+                }
+            }
+
+            return (dp[amount] >= INF) ? -1 : dp[amount];
     }
 };
