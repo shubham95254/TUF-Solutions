5f5cf648a63e5c2993d80771428bfd4a752c7e51
Shubham <125039017+shubham95254@users.noreply.github.com>
2026-08-04 13:29:50 +0530

    Add solution for Minimum coins - TUFHub

--- DSA/General/minimum-coins/solution.cpp
+++ DSA/General/minimum-coins/solution.cpp
@@ -1,17 +1,28 @@
 class Solution{
 	public:
     int f(int ind, int totalrem, vector<int>&coins, vector<vector<int>>&dp){
-        if(ind<0) return 100000;
-        if(totalrem==0) return 0;
+        // if(ind<0) return 100000;
+        // if(totalrem==0) return 0;
+
+        dp[0][0] = 0;
+        for(int j=1; j<=totalrem; j++){
+            dp[0][j] = (j%coins[0]==0)? j/coins[0] : 1e9;
+        }
         
-        if(dp[ind][totalrem]!=-1) return dp[ind][totalrem];
+        // if(dp[ind][totalrem]!=-1) return dp[ind][totalrem];
+        for(int i=1; i<=ind; i++) {
+            dp[i][0] = 0;
+            for(int j=1; j<=totalrem; j++) {
+                int take = 100000;
+                if(coins[i]<=j) take = 1+dp[i][j-coins[i]];
+                int nottake = dp[i-1][j];
 
-        int take = 100000;
-        if(coins[ind]<=totalrem) take = f(ind, totalrem-coins[ind], coins, dp)+1;
-        int nottake = f(ind-1, totalrem, coins, dp);
+                // cout << "f(" <<ind<<","<<totalrem<< ") " << take << " " << nottake<<endl;
+                dp[i][j]= min(take, nottake);
+            }
+        }
+        return dp[ind][totalrem];
 
-        cout << "f(" <<ind<<","<<totalrem<< ") " << take << " " << nottake<<endl;
-        return dp[ind][totalrem]= min(take, nottake);
 
     }
 	int MinimumCoins(vector<int>& coins, int amount) {
