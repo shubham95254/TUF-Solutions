d30929bf5f847b1184d0f912f165f849bebe56f9
Shubham <125039017+shubham95254@users.noreply.github.com>
2026-08-10 13:58:32 +0530

    Add solution for Detect a cycle in an undirected graph - TUFHub

--- DSA/Graphs/detect-a-cycle-in-an-undirected-graph/DFS.cpp
+++ DSA/Graphs/detect-a-cycle-in-an-undirected-graph/DFS.cpp
@@ -1,6 +1,28 @@
-class Solution{
+class Solution{
 public:
-    bool isCycle(int V, vector<int> adj[]) {
-        
-    }
+    bool traverse(int node, int parent, vector<int> adj[], vector<bool> &vis){
+        vis[node] = true;
+        
+        for(auto it : adj[node]){
+                if(vis[it] && it!=parent) {
+                    return true;
+                }
+                if(!vis[it]){
+                    if(traverse(it, node, adj, vis)) return true;
+                    
+                }
+        }
+        return false;
+
+    }
+    bool isCycle(int V, vector<int> adj[]) {
+        vector<bool> vis(V, false);
+        
+        for(int i=0; i<V; i++) {
+            if(!vis[i]){
+                if(traverse(i, -1, adj, vis)) return true;
+            }
+        }
+        return false;
+    }
 };
