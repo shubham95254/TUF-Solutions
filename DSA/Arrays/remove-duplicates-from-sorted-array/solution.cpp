bcfaf1fd5e18aea48c97ab16f870f1c866333b12
Shubham <125039017+shubham95254@users.noreply.github.com>
2026-09-01 12:02:03 +0530

    Sync Remove duplicates from sorted array [DSA] - TUFHub

--- DSA/Arrays/remove-duplicates-from-sorted-array/solution.cpp
+++ DSA/Arrays/remove-duplicates-from-sorted-array/solution.cpp
@@ -1,18 +1,11 @@
-class Solution {
+class Solution {
 public:
-    int removeDuplicates(vector<int>& nums) {
-        int n  = nums.size();
-        if(n<=1) return n;
-        int count = 1;
-        int i = 0, j = 1;
-        while(j<n){
-            if(nums[i]!=nums[j]){
-                i++;count++;
-                nums[i] = nums[j];
-            }
-            j++;
-        }
-        return count;
-
-    }
-};
+    int removeDuplicates(vector<int>& nums) {
+        int n  = nums.size();
+        if(n<=1) return n;
+        int count = 1;
+        int i = 0, j = 1;
+        while(j<n){
+            if(nums[i]!=nums[j]){
+                i++;count++;
+                nums[i] = nums[j];
