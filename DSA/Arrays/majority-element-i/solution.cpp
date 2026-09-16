7c1c51020e034c79f83f9fae609f242086601375
Shubham <125039017+shubham95254@users.noreply.github.com>
2026-09-16 11:57:53 +0530

    Sync Majority Element-I [DSA] - TUFHub

--- DSA/Arrays/majority-element-i/solution.cpp
+++ DSA/Arrays/majority-element-i/solution.cpp
@@ -1,7 +1,12 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
+        // brute - for each element in arr, check its count
+        //better - use hashmap for frequency count
+        //optimal - use Moore's Voting Algorithm
+
         int num , count = 0, n = nums.size();
+        //Moore's Voting Algorithm
         for(int i=0;i<n; i++){
             if(count==0) {
                 count++;
@@ -11,7 +16,7 @@ public:
                 else count--;
             }
         }
-        if(count==0) return -1;
+        if(count==0) return -1; //no element can be in majority
 
         //check if given number is majorityElement or not
         count=0;
@@ -19,5 +24,6 @@ public:
             if(it==num) count++;
         }
         if(count>(n/2)) return num;
+        return -1;
     }
 };
